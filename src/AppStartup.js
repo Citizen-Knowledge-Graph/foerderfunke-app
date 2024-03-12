21211a5755af2bef6fe8352f618b688b3a360dc5
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-03-12 22:06:17 +0100

    Include our matching library (empty so far)

--- src/AppStartup.js
+++ src/AppStartup.js
@@ -3,6 +3,7 @@ import {useDispatch} from 'react-redux';
 import fetchDataToDevice from './controllers/dataFetching';
 import runValidation from './controllers/validation';
 import loadDataIntoRedux from './controllers/dataService';
+import {func} from '@foerderfunke/matching-engine';
 
 const AppStartup = ({children}) => {
   const dispatch = useDispatch();
@@ -17,6 +18,8 @@ const AppStartup = ({children}) => {
 
       // Run initial validation
       await runValidation(dispatch);
+
+      func();
     };
 
     initializeData();
