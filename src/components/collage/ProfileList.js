29a185d8c427ede145875eae2dc8d42d77b3f44a
wbglaeser <ben.glaeser@tuta.io>
2024-03-14 16:13:35 +0100

    fix up datafield naming

--- src/components/collage/ProfileList.js
+++ src/components/collage/ProfileList.js
@@ -5,18 +5,24 @@ import {retrieveAttribute} from '../../utilities/graphManagement';
 
 const ProfileList = ({profileData}) => {
   const dataFields = [
-    'hasBirthday',
-    'hasResidence',
-    'hasDrivingLicense',
-    'hasChildren',
+    ['hasBirthday', 'Birthday'],
+    ['hasResidence', 'Residence'],
+    ['hasDrivingLicense', 'Driving License'],
+    ['hasChildren', 'Children'],
   ];
 
   return (
     <View style={styles.container}>
       {dataFields.map((field, index) => {
-        const targetNodes = retrieveAttribute(profileData, 'citizen-a', field);
+        const targetNodes = retrieveAttribute(
+          profileData,
+          'citizen-a',
+          field[0],
+        );
         const value = Array.from(targetNodes)[0].object.value;
-        return <ProfileListItem key={index} category={field} value={value} />;
+        return (
+          <ProfileListItem key={index} category={field[1]} value={value} />
+        );
       })}
     </View>
   );
@@ -29,14 +35,3 @@ const styles = StyleSheet.create({
 });
 
 export default ProfileList;
-
-// <UserItem category="age" value={userData.age} variant="blue" />
-// <UserItem category="residence" value={userData.residence} variant="" />
-// <UserItem
-//   category="profession"
-//   value={userData.profession}
-//   variant="blue"
-// />
-// <UserItem category="married" value={userData.married} variant="" />
-// <UserItem category="children" value={userData.children} variant="blue" />
-// <UserItem category="company" value={userData.company} variant="" />
