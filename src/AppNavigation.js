aba7020fd09c83e0a3c785c01fb1f1133b3f05e4
wbglaeser <ben.glaeser@tuta.io>
2024-03-14 17:44:09 +0100

    add icons

--- src/AppNavigation.js
+++ src/AppNavigation.js
@@ -1,6 +1,6 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import StartScreen from './screens/StartScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -50,20 +50,21 @@ const Tab = createBottomTabNavigator();
 export const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: '#fff',
-          borderTopWidth: 0,
-          marginBottom: 5,
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'user' : 'user';
+          }
+          return <FontAwesomeIcon name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: '#007bff', // Color of the icon and text when the tab is active
-        tabBarInactiveTintColor: '#8e8e93', // Color of the icon and text when the tab is inactive
-      }}>
+      })}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={({route}) => ({
-          // Include route in the function parameters
           headerShown: false,
         })}
       />
@@ -71,10 +72,19 @@ export const BottomTabNavigator = () => {
         name="Profile"
         component={ProfileScreen}
         options={({route}) => ({
-          // Include route in the function parameters
           headerShown: false,
         })}
       />
     </Tab.Navigator>
   );
 };
+
+// {{
+//   tabBarStyle: {
+//     backgroundColor: '#fff',
+//       borderTopWidth: 0,
+//       marginBottom: 5,
+//   },
+//   tabBarActiveTintColor: '#007bff',
+//     tabBarInactiveTintColor: '#8e8e93',
+// }}
