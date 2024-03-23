ebb3cc0dad360193d478a0cd3d3889bea5bd4c78
wbglaeser <ben.glaeser@tuta.io>
2024-03-23 19:20:26 +0100

    remove clutter

--- src/storage/reducers/index.js
+++ src/storage/reducers/index.js
@@ -1,13 +1,15 @@
 import {combineReducers} from 'redux';
 import validationReducer from './validationReducer';
 import queriesReducer from './queriesReducer';
-import guidesReducer from './guidesReducer';
-import userReducer from './userReducer';
+
+gimport;
+userReducer;
+from;
+('./userReducer');
 
 const rootReducer = combineReducers({
   validationReducer: validationReducer,
   queriesReducer: queriesReducer,
-  guidesReducer: guidesReducer,
   userReducer: userReducer,
 });
 
