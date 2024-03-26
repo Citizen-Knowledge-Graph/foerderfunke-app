27eb1e6831d338c58f2e76ad598522753af54af0
wbglaeser <ben.glaeser@tuta.io>
2024-03-26 20:16:51 +0100

    remove typo

--- src/controllers/dataFetching.js
+++ src/controllers/dataFetching.js
@@ -7,8 +7,7 @@ const fetchDataToDevice = async () => {
   const binaryData = await fetchZipAsset(require('../../assets/data.zip'));
 
   // Unzip data
-  const unzippedData = await unzipFromBase64(binaryData);
-  g;
+  const unzippedData = await unzipFromBase64(binaryData)g;
 
   // Write unzipped data to device
   for (const file of unzippedData) {
