305400a10aed7e5abb81fa97906dca2106fa5f00
wbglaeser <ben.glaeser@tuta.io>
2024-03-27 10:10:29 +0100

    remove some clutter comments

--- src/controllers/dataFetching.js
+++ src/controllers/dataFetching.js
@@ -1,15 +1,9 @@
 import { writeFile, fetchZipAsset } from '../utilities/fileManagement';
 import { unzipFromBase64 } from '../utilities/zipHandling';
 
-// Fetch all data and copy it to device
 const fetchDataToDevice = async () => {
-  // Fetch zipped data and wrangle to binary
   const binaryData = await fetchZipAsset(require('../../assets/data.zip'));
-
-  // Unzip data
   const unzippedData = await unzipFromBase64(binaryData);
-
-  // Write unzipped data to device
   for (const file of unzippedData) {
     console.log(file.filename);
     await writeFile(file.filename, file.fileContent, true);
