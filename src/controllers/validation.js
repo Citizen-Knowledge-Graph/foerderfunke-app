687f80195bcaf2b5b8386abb01c57175288e146b
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-03-29 12:26:10 +0100

    Test-use matching-engine library

--- src/controllers/validation.js
+++ src/controllers/validation.js
@@ -6,6 +6,7 @@ import {
   parseTurtle,
 } from '../utilities/rdfHandling.js';
 import validationReportAction from '../storage/actions/validationReport.js';
+import { validateUserProfile } from '@foerderfunke/matching-engine';
 
 /**
  * Create report for profile
@@ -19,6 +20,10 @@ const createValidationReport = async (shapes, profile) => {
 const runValidation = async dispatch => {
   // set up filepaths
   const userProfilePath = 'user-profile.ttl';
+
+  let report = validateUserProfile(userProfilePath);
+  console.log(report);
+
   const enitityValidationRegistryPath = 'entity-registry.json';
   const queryRegistryPath = 'query-registry.json';
 
