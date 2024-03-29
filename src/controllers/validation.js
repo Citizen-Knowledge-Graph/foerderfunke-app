d7196add37e9c0037b7896d3e19b9d03af92dd39
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-03-29 22:36:23 +0100

    First working usage of matching-engine library

--- src/controllers/validation.js
+++ src/controllers/validation.js
@@ -1,41 +1,46 @@
 import rdfDataModel from '@rdfjs/data-model';
 import Validator from 'shacl-engine/Validator.js';
-import {readFile, readJson} from '../utilities/fileManagement.js';
+import { readFile, readJson } from '../utilities/fileManagement.js';
 import {
   combineTurtleStringsIntoDataset,
   parseTurtle,
 } from '../utilities/rdfHandling.js';
 import validationReportAction from '../storage/actions/validationReport.js';
+import { validateUserProfile } from '@foerderfunke/matching-engine';
 
 /**
  * Create report for profile
  */
 const createValidationReport = async (shapes, profile) => {
-  const validator = new Validator(shapes, {factory: rdfDataModel});
-  return await validator.validate({dataset: profile});
+  const validator = new Validator(shapes, { factory: rdfDataModel });
+  return await validator.validate({ dataset: profile });
 };
 
 // run validation
-const runValidation = async dispatch => {
-  // set up filepaths
+const runValidation = async (dispatch) => {
   const userProfilePath = 'user-profile.ttl';
-  const enitityValidationRegistryPath = 'entity-registry.json';
+  const datafieldsPath = 'datafields.ttl';
+  const entityValidationRegistryPath = 'entity-registry.json';
   const queryRegistryPath = 'query-registry.json';
 
-  // load user profile to shapes
   const userProfileString = await readFile(userProfilePath);
+  const datafieldsString = await readFile(datafieldsPath);
+
+  let result = await validateUserProfile(userProfileString, datafieldsString);
+  console.log(result);
+
   const userProfile = await parseTurtle(userProfileString);
 
   // load user validation to shapes
-  const enitityValidationRegistry = await readJson(
-    enitityValidationRegistryPath,
+  const entityValidationRegistry = await readJson(
+    entityValidationRegistryPath,
   );
 
-  // Iterate through entity registry
+  // iterate through entity registry
   const entityShapesArray = [];
-  for (let key in enitityValidationRegistry) {
-    if (enitityValidationRegistry.hasOwnProperty(key)) {
-      const constraintsPath = enitityValidationRegistry[key].path;
+  for (let key in entityValidationRegistry) {
+    if (entityValidationRegistry.hasOwnProperty(key)) {
+      const constraintsPath = entityValidationRegistry[key].path;
       const entityString = await readFile(constraintsPath);
       entityShapesArray.push(entityString);
     }
@@ -51,7 +56,7 @@ const runValidation = async dispatch => {
   // load query registry
   const queryRegistry = await readJson(queryRegistryPath);
 
-  // Iterate through queries gitregistry
+  // iterate through queries gitregistry
   for (let key in queryRegistry) {
     if (queryRegistry.hasOwnProperty(key)) {
       console.log('Running validation for: ', key);
