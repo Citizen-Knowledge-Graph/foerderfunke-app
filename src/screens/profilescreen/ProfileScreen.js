dac96586aea14a25d4547772773e92194a8892f1
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-03-30 18:23:15 +0100

    Add export button to profile

--- src/screens/profilescreen/ProfileScreen.js
+++ src/screens/profilescreen/ProfileScreen.js
@@ -1,13 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-native';
 import ScreenView from '../../components/ScreenView';
 import UserProfile from './components/UserProfile';
-import {useSelector} from 'react-redux';
-import {fetchProfileScreenData} from './ProfileScreenController';
+import { useSelector } from 'react-redux';
+import { fetchProfileScreenData } from './ProfileScreenController';
+import Share from 'react-native-share';
+import * as FileSystem from 'expo-file-system';
 
 // Component
 const ProfileScreen = () => {
   const [profileScreenData, setProfileScreenData] = useState(null);
-  const userState = useSelector(state => state.userReducer);
+  const userState = useSelector((state) => state.userReducer);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,11 +25,28 @@ const ProfileScreen = () => {
     fetchData();
   }, [userState]);
 
+  const shareFile = async () => {
+    const userProfilePath = 'user-profile.ttl';
+    const absoluteFilePath = FileSystem.documentDirectory + userProfilePath;
+    const shareOptions = {
+      url: `file://${absoluteFilePath}`,
+      type: 'text/turtle',
+    };
+    try {
+      await Share.open(shareOptions)
+        .then((res) => console.log('Share was successful', res))
+        .catch((err) => console.log('Error sharing the file', err));
+    } catch (error) {
+      console.error(`Error accessing file: ${absoluteFilePath}`, error);
+    }
+  };
+
   return (
     <ScreenView screenName={'Profile'}>
       {profileScreenData ? (
         <UserProfile profileScreenData={profileScreenData} />
       ) : null}
+      <Button onPress={shareFile} title='Export' />
     </ScreenView>
   );
 };
