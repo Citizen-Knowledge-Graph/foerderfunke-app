41f6d771b8c9968dc31a9867ad7d60062475440d
wbglaeser <ben.glaeser@tuta.io>
2024-04-02 09:41:32 +0200

    refactor bottom view

--- src/screens/profilescreen/components/BottomView.js
+++ src/screens/profilescreen/components/BottomView.js
@@ -1,6 +1,5 @@
-import React, { useMemo, useCallback, useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { Card, styled, Button, SizableText, View } from 'tamagui';
 import { ChevronDown } from '@tamagui/lucide-icons';
 import {
@@ -10,31 +9,21 @@ import {
   BottomSheetBackdrop,
   useBottomSheetModal,
 } from '@gorhom/bottom-sheet';
-
 import { colorTokens } from '@tamagui/themes';
 import { performUpdate } from '../../../storage/actions/userReport';
 
 const BottomView = ({ bottomSheetModalRef, currentEntry }) => {
-  const [inputText, setInputText] = useState(currentEntry?.object.value);
-
-  useEffect(() => {
-    setInputText(currentEntry?.object.value);
-  }, [currentEntry]);
-
+  const [inputText, setInputText] = useState('');
   const dispatch = useDispatch();
-
   const snapPoints = useMemo(() => ['50%'], []);
   const { dismiss } = useBottomSheetModal();
 
-  const renderBackdrop = useCallback(
-    (props) => (
-      <BottomSheetBackdrop
-        {...props}
-        disappearsOnIndex={-1}
-        appearsOnIndex={0}
-      />
-    ),
-    []
+  useEffect(() => {
+    setInputText(currentEntry?.object?.value || '');
+  }, [currentEntry?.object?.value]);
+
+  const renderBackdrop = (props) => (
+    <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={0} />
   );
 
   return (
@@ -42,53 +31,21 @@ const BottomView = ({ bottomSheetModalRef, currentEntry }) => {
       ref={bottomSheetModalRef}
       index={0}
       snapPoints={snapPoints}
-      enablePanDownToClose={true}
+      enablePanDownToClose
       backdropComponent={renderBackdrop}
       handleIndicatorStyle={{ backgroundColor: 'white' }}
     >
       <BottomSheetView>
         <UpdateCard>
-          <Button
-            size="$4"
-            circular
-            icon={<ChevronDown size="$2" />}
-            onPress={() => dismiss()}
-            marginVertical={16}
-            backgroundColor={colorTokens.light.gray.gray8}
-          />
-          <SizableText size="$6" color={'black'} fontWeight={'500'}>
-            Update your Userprofile
-          </SizableText>
-          <View borderBottomWidth={1}>
-            <SizableText size="$6" color={'black'} marginTop={16}>
-              {currentEntry?.displayName}
-            </SizableText>
-          </View>
-          <BottomSheetTextInput
-            backgroundColor={colorTokens.light.gray.gray4}
-            borderRadius={5}
-            padding={16}
-            minWidth={200}
-            fontSize={18}
-            marginVertical={16}
-            textAlign="center"
-            onChangeText={(text) => setInputText(text)}
-          >
-            {currentEntry?.object?.value}
-          </BottomSheetTextInput>
-          <Button
-            size="$4"
+          <DismissButton onPress={dismiss} />
+          <UserProfile currentEntry={currentEntry} />
+          <ProfileTextInput value={inputText} onChangeText={setInputText} />
+          <UpdateButton
             onPress={() => {
               dispatch(performUpdate(currentEntry, inputText));
               dismiss();
             }}
-            marginVertical={16}
-            backgroundColor={colorTokens.light.purple.purple8}
-            color={colorTokens.light.gray.gray1}
-            fontSize={18}
-          >
-            Update
-          </Button>
+          />
         </UpdateCard>
       </BottomSheetView>
     </BottomSheetModal>
@@ -103,5 +60,57 @@ const UpdateCard = styled(Card, {
   justifyContent: 'flex-start',
   alignItems: 'center',
 });
+
+// Here we define small functional components for parts of our UI
+const DismissButton = ({ onPress }) => (
+  <Button
+    size="$4"
+    circular
+    icon={<ChevronDown size="$2" />}
+    onPress={onPress}
+    marginVertical={16}
+    backgroundColor={colorTokens.light.gray.gray8}
+  />
+);
+
+const UserProfile = ({ currentEntry }) => (
+  <>
+    <SizableText size="$6" color="black" fontWeight="500">
+      Update your Userprofile
+    </SizableText>
+    <View borderBottomWidth={1}>
+      <SizableText size="$6" color="black" marginTop={16}>
+        {currentEntry?.displayName}
+      </SizableText>
+    </View>
+  </>
+);
+
+const ProfileTextInput = ({ value, onChangeText }) => (
+  <BottomSheetTextInput
+    value={value}
+    onChangeText={onChangeText}
+    backgroundColor={colorTokens.light.gray.gray4}
+    borderRadius={5}
+    padding={16}
+    minWidth={200}
+    fontSize={18}
+    marginVertical={16}
+    textAlign="center"
+  />
+);
+
+const UpdateButton = ({ onPress }) => (
+  <Button
+    size="$4"
+    onPress={onPress}
+    marginVertical={16}
+    backgroundColor={colorTokens.light.purple.purple8}
+    color={colorTokens.light.gray.gray1}
+    fontSize={18}
+  >
+    Update
+  </Button>
+);
 
 export default BottomView;
