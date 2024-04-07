59134233b315cc6f159f3f7b53df4f7a0193cad2
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-04-07 23:47:38 +0200

    Start implementing the pull logic from repo

--- src/controllers/dataFetching.js
+++ src/controllers/dataFetching.js
@@ -7,6 +7,7 @@ import {
 } from '../utilities/fileManagement';
 import { unzipFromBase64 } from '../utilities/zipHandling';
 import * as FileSystem from 'expo-file-system';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const fetchDataToDevice = async () => {
   // 1. Delete all files: clean slate
@@ -25,8 +26,37 @@ const fetchDataToDevice = async () => {
     await writeFile(file.filename, file.fileContent, true);
   }
 
-  // 3. Download from requirement-profiles repo
+  // 3. Download from requirement-profiles repo OR update/add/delete files that changed since we last checked
 
+  await downloadAndUnpackRequirementsProfileRepo();
+
+  let storedLatestCommit = await AsyncStorage.getItem('requirement-profiles-repo-latest-commit');
+  if (storedLatestCommit == null) {
+    // await downloadAndUnpackRequirementsProfileRepo();
+    // store latest commit TODO
+  } else {
+    // check if latest commit is different to stored one, if yes, extract changed files and download them one by one
+    /*
+    url = `https://api.github.com/repos/Citizen-Knowledge-Graph/requirement-profiles/compare/${firstCommit}...${latestCommit}`;
+    response = await fetch(url);
+    let changedFiles = (await response.json()).files.map(file => ({
+      filename: file.filename,
+      status: file.status,
+    }));
+    changedFiles = changedFiles.filter((file) => file.filename.endsWith('.ttl'));
+    // TODO
+    */
+  }
+  let commitsUrl = 'https://api.github.com/repos/Citizen-Knowledge-Graph/requirement-profiles/commits?per_page=1';
+  let response = await fetch(commitsUrl);
+  let latestCommit = (await response.json()).map((commit) => commit.sha)[0];
+  console.log('Latest commit:', latestCommit);
+  await AsyncStorage.setItem('requirement-profiles-repo-latest-commit', latestCommit);
+
+  console.log('All files in app storage:', await listAllFiles(true));
+};
+
+const downloadAndUnpackRequirementsProfileRepo = async () => {
   const downloadUrl =
     'https://github.com/Citizen-Knowledge-Graph/requirement-profiles/archive/main.zip';
   const targetLocation = FileSystem.documentDirectory + 'main.zip';
@@ -35,8 +65,8 @@ const fetchDataToDevice = async () => {
     targetLocation
   );
 
-  binaryData = await fetchZipAssetFromFileUri(actualLocation);
-  unzippedData = await unzipFromBase64(binaryData);
+  let binaryData = await fetchZipAssetFromFileUri(actualLocation);
+  let unzippedData = await unzipFromBase64(binaryData);
 
   console.log('Adding files from repo main.zip:');
   for (const file of unzippedData) {
@@ -49,8 +79,6 @@ const fetchDataToDevice = async () => {
 
   // delete main.zip
   await FileSystem.deleteAsync(actualLocation);
-
-  console.log('All files in app storage:', await listAllFiles(true));
 };
 
 export default fetchDataToDevice;
