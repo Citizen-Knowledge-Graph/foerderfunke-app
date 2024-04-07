640913650f1064f84a052f6df84bf7068e0d33a9
wbglaeser <ben.glaeser@tuta.io>
2024-04-07 20:53:50 +0200

    remove unused guides assets

--- src/controllers/dataFetching.js
+++ src/controllers/dataFetching.js
@@ -1,9 +1,15 @@
-import { writeFile, fetchZipAssetFromModule, fetchZipAssetFromFileUri } from '../utilities/fileManagement';
+import {
+  writeFile,
+  fetchZipAssetFromModule,
+  fetchZipAssetFromFileUri,
+} from '../utilities/fileManagement';
 import { unzipFromBase64 } from '../utilities/zipHandling';
 import * as FileSystem from 'expo-file-system';
 
 const fetchDataToDevice = async () => {
-  let binaryData = await fetchZipAssetFromModule(require('../../assets/data.zip'));
+  let binaryData = await fetchZipAssetFromModule(
+    require('../../assets/data.zip')
+  );
   let unzippedData = await unzipFromBase64(binaryData);
 
   for (const file of unzippedData) {
@@ -11,15 +17,21 @@ const fetchDataToDevice = async () => {
     await writeFile(file.filename, file.fileContent, true);
   }
 
-  const downloadUrl = 'https://github.com/Citizen-Knowledge-Graph/requirement-profiles/archive/main.zip';
+  const downloadUrl =
+    'https://github.com/Citizen-Knowledge-Graph/requirement-profiles/archive/main.zip';
   const targetLocation = FileSystem.documentDirectory + 'main.zip';
-  const { uri: actualLocation } = await FileSystem.downloadAsync(downloadUrl, targetLocation);
+  const { uri: actualLocation } = await FileSystem.downloadAsync(
+    downloadUrl,
+    targetLocation
+  );
 
   binaryData = await fetchZipAssetFromFileUri(actualLocation);
   unzippedData = await unzipFromBase64(binaryData);
 
   for (const file of unzippedData) {
-    if (!file.filename.endsWith('.ttl')) continue;
+    if (!file.filename.endsWith('.ttl')) {
+      continue;
+    }
     let filename = file.filename.split('/').pop(); // remove "requirement-profiles-main/" from the beginning
     console.log('from repo main.zip:', filename);
     await writeFile(filename, file.fileContent, true);
