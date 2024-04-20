5ef23c122405559056e9685ddf748d85c7d329ca
wbglaeser <ben.glaeser@tuta.io>
2024-04-20 11:55:52 +0200

    add different input types

---
+++ src/screens/onboarding/components/InputSections.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Platform, StyleSheet, TextInput } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Button, View } from 'tamagui';
+
+export const StringInput = ({ setInputData }) => (
+  <TextInput size='$4' style={styles.inputField} onChangeText={setInputData} />
+);
+
+export const IntegerInput = ({ setInputData }) => {
+  const handleTextChange = (text) => {
+    if (/^\d*$/.test(text)) {
+      setInputData(text);
+    }
+  };
+
+  return (
+    <TextInput
+      size='$4'
+      style={styles.inputField}
+      onChangeText={handleTextChange}
+      keyboardType='numeric'
+      placeholder='Enter an integer'
+    />
+  );
+};
+
+export const DateInput = ({ setInputData }) => {
+  const [date, setDateInternal] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios'); // Keep the picker open on iOS
+    setDateInternal(currentDate);
+    setInputData(currentDate); // Assuming setDate is a prop for lifting state up
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  return (
+    <View>
+      <View style={styles.button}>
+        <Button onPress={() => showMode('date')} title='Show Date Picker' />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID='dateTimePicker'
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display='default'
+          onChange={onChange}
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputField: {
+    height: 40,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  button: {
+    marginBottom: 10,
+  },
+});
