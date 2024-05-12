345008b490c86faf6f9675b790af826f5c0caddc
wbglaeser <ben.glaeser@tuta.io>
2024-05-12 18:39:55 +0200

    simplify drop down

--- src/screens/profilescreen/components/UserSelection.js
+++ src/screens/profilescreen/components/UserSelection.js
@@ -1,91 +1,44 @@
 import React, { useState } from 'react';
-import { Select, Adapt, Sheet } from 'tamagui';
+import { SizableText, View } from 'tamagui';
 import { colorTokens } from '@tamagui/themes';
-import { Check, ChevronDown } from '@tamagui/lucide-icons';
 import { useUserStore } from '../../../storage/zustand';
+import DropDownPicker from 'react-native-dropdown-picker';
 
-const UserSelection = ({ alternativeUserProfiles }) => {
-  const userId = useUserStore((state) => state.userId);
+export const UserSelection = ({ alternativeUserProfiles }) => {
+  const { userId, updateUserId } = useUserStore();
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(userId);
+  const [items, setItems] = useState(
+    alternativeUserProfiles.map((obj) => ({
+      label: obj,
+      value: obj,
+    }))
+  );
 
   return (
-    <SelectDemoItem
-      id='select-demo-1'
-      selectedUser={userId}
-      alternativeUserProfiles={alternativeUserProfiles}
-    />
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 15,
+      }}
+    >
+      <SizableText>Select a different user profile</SizableText>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        onSelectItem={(item) => {
+          updateUserId(item.value);
+        }}
+        listMode='SCROLLVIEW'
+      />
+    </View>
   );
 };
-
-export function SelectDemoItem(props) {
-  const { userId, updateUserId } = useUserStore();
-  const [val, setVal] = useState(userId);
-
-  return (
-    <Select
-      value={val}
-      onValueChange={(value) => {
-        setVal(value);
-        updateUserId(value);
-      }}
-      disablePreventBodyScroll
-      {...props}
-    >
-      <Select.Trigger
-        iconAfter={<ChevronDown size='$1' color={'black'} />}
-        style={styles.userSelectionHeaderView}
-      >
-        <Select.Value
-          placeholder='Select a different User Profile'
-          style={styles.userSelectionTrigger}
-        />
-      </Select.Trigger>
-
-      <Adapt platform='touch'>
-        <Sheet
-          snapPoints={[50]}
-          native={!!props.native}
-          modal
-          dismissOnSnapToBottom
-          animationConfig={{
-            type: 'spring',
-            damping: 20,
-            mass: 1.2,
-            stiffness: 250,
-          }}
-        >
-          <Sheet.Frame>
-            <Sheet.ScrollView>
-              <Adapt.Contents />
-            </Sheet.ScrollView>
-          </Sheet.Frame>
-          <Sheet.Overlay
-            animation='lazy'
-            enterStyle={{ opacity: 0 }}
-            exitStyle={{ opacity: 0 }}
-          />
-        </Sheet>
-      </Adapt>
-
-      <Select.Content zIndex={200000}>
-        <Select.Viewport minWidth={200}>
-          <Select.Group>
-            <Select.Label>User Profile</Select.Label>
-            {props.alternativeUserProfiles.map((item, i) => {
-              return (
-                <Select.Item index={i} key={item} value={item.toLowerCase()}>
-                  <Select.ItemText>{item}</Select.ItemText>
-                  <Select.ItemIndicator marginLeft='auto'>
-                    <Check size={16} />
-                  </Select.ItemIndicator>
-                </Select.Item>
-              );
-            })}
-          </Select.Group>
-        </Select.Viewport>
-      </Select.Content>
-    </Select>
-  );
-}
 
 export default UserSelection;
 const styles = {
